950083685d60864c2e956f8d9abf770df9cbfdb4
agavrilov <agavrilov@rit-it.com>
2024-10-23 15:30:28 +0700

    Поменял цвет

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -126,7 +126,7 @@ const ProjectsSection = () => {
   };
 
   return (
-    <section id="projects">
+    <section id="projects" className="text-white">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         Проект
       </h2>
